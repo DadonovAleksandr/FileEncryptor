7fa7827a9c70610b8d715ac2279b6709cd3be6d3
DadonovAD <DadonovAD@tvv.transneft.ru>
2023-08-16 15:50:59 +0300

    Команды

---
+++ FileEncryptor.WPF/Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace FileEncryptor.WPF.Infrastructure.Commands.Base;
+
+internal abstract class Command : ICommand
+{
+    private bool _Executable;
+    public bool Executable
+    {
+        get => _Executable; 
+        set
+        {
+            if(_Executable == value) return;
+            _Executable = value;
+            CommandManager.InvalidateRequerySuggested();
+        }
+    }
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    bool ICommand.CanExecute(object? parameter) => _Executable && CanExecute(parameter);
+    void ICommand.Execute(object? parameter)
+    {
+        if (CanExecute(parameter))
+            Execute(parameter);
+    }
+
+    protected virtual bool CanExecute(object? parameter) => true;
+
+    protected abstract void Execute(object? parameter);
+
+}
