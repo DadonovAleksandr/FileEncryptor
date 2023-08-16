99420ce893a6c7523a1c0ace55285eca0aa8d72b
DadonovAD <DadonovAD@tvv.transneft.ru>
2023-08-16 15:14:10 +0300

    Модель-предстовление

---
+++ FileEncryptor.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using FileEncryptor.WPF.ViewModels.Base;
+
+namespace FileEncryptor.WPF.ViewModels;
+
+internal class MainWindowViewModel : ViewModel
+{
+    #region Title
+    private string _title = "Шифратор";
+    public string Title
+    {
+        get { return _title; }
+        set { Set(ref _title, value); }
+    }
+    #endregion
+}
