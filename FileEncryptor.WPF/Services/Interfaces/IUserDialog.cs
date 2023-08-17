4ba10e026797923e801ed75ab9bf025f664d6149
DadonovAD <DadonovAD@tvv.transneft.ru>
2023-08-17 10:26:35 +0300

    Диалог с пользователем

--- FileEncryptor.WPF/Services/Interfaces/IUserDialog.cs
+++ FileEncryptor.WPF/Services/Interfaces/IUserDialog.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace FileEncryptor.WPF.Services.Interfaces
+namespace FileEncryptor.WPF.Services.Interfaces;
+
+interface IUserDialog
 {
-    interface IUserDialog
-    {
-    }
+    bool OpenFile(string title, out string selectedFile, string filter = "Все файлы (*.*)|*.*");
+    bool OpenFiles(string title, out IEnumerable<string> selectedFiles, string filter = "Все файлы (*.*)|*.*");
 }
